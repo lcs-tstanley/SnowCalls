51ea9821333d359f3e3e5b137183ac38712ed0a1
Trent Stanley <tstanley@lcs.on.ca>
2019-12-17 15:09:43 -0500

    Got an individual character to convert

--- SnowCalls/ViewController.swift
+++ SnowCalls/ViewController.swift
@@ -27,32 +27,38 @@ class ViewController: UIViewController {
         
         switch inputNumbers {
             
+        case "1":
+            outputNumbers.text = "1"
+            
         case "A","B","C","2":
-            outputNumbers.text! += "2"
+            outputNumbers.text = "2"
             
         case "D","E","F","3":
-            outputNumbers.text! += "3"
+            outputNumbers.text = "3"
             
         case "G","H","I","4":
-            outputNumbers.text! += "4"
+            outputNumbers.text = "4"
             
         case "J","K","L","5":
-            outputNumbers.text! += "5"
+            outputNumbers.text = "5"
             
         case "M","N","O","6":
-            outputNumbers.text! += "6"
+            outputNumbers.text = "6"
             
         case "P","Q","R","S","7":
-            outputNumbers.text! += "7"
+            outputNumbers.text = "7"
             
         case "T","U","V","8":
-            outputNumbers.text! += "8"
+            outputNumbers.text = "8"
             
         case "W","X","Y","Z","9":
-            outputNumbers.text! += "9"
+            outputNumbers.text = "9"
+            
+        case "0":
+            outputNumbers.text = "0"
             
         default:
-            outputNumbers.text! += ""
+            outputNumbers.text = "wrong"
             
         }
         
