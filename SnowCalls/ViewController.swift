3e14f6889e951466012b50a69547f92d6314c657
Trent Stanley <tstanley@lcs.on.ca>
2020-01-25 19:24:02 -0500

    Indenting

--- SnowCalls/ViewController.swift
+++ SnowCalls/ViewController.swift
@@ -25,42 +25,49 @@ class ViewController: UIViewController {
             outputNumbers.text = "Please enter some phone numbers."
             return }
         
-        switch inputNumbers {
+        // Break up the string into individual characters and process one by one
+        var translatedPhoneNumber = ""
+        for character in inputNumbers {
             
-        case "1":
-            outputNumbers.text = "1"
-            
-        case "A","B","C","2":
-            outputNumbers.text = "2"
-            
-        case "D","E","F","3":
-            outputNumbers.text = "3"
-            
-        case "G","H","I","4":
-            outputNumbers.text = "4"
-            
-        case "J","K","L","5":
-            outputNumbers.text = "5"
-            
-        case "M","N","O","6":
-            outputNumbers.text = "6"
-            
-        case "P","Q","R","S","7":
-            outputNumbers.text = "7"
-            
-        case "T","U","V","8":
-            outputNumbers.text = "8"
-            
-        case "W","X","Y","Z","9":
-            outputNumbers.text = "9"
-            
-        case "0":
-            outputNumbers.text = "0"
-            
-        default:
-            outputNumbers.text = "wrong"
+            switch character {
+                
+            case "1":
+                translatedPhoneNumber += "1"
+                
+            case "A","B","C","2":
+                translatedPhoneNumber += "2"
+                
+            case "D","E","F","3":
+                translatedPhoneNumber += "3"
+                
+            case "G","H","I","4":
+                translatedPhoneNumber += "4"
+                
+            case "J","K","L","5":
+                translatedPhoneNumber += "5"
+                
+            case "M","N","O","6":
+                translatedPhoneNumber += "6"
+                
+            case "P","Q","R","S","7":
+                translatedPhoneNumber += "7"
+                
+            case "T","U","V","8":
+                translatedPhoneNumber += "8"
+                
+            case "W","X","Y","Z","9":
+                translatedPhoneNumber += "9"
+                
+            case "0":
+                translatedPhoneNumber += "0"
+                
+            default:
+                translatedPhoneNumber += "wrong"
+                
+            }
             
         }
+        outputNumbers.text = translatedPhoneNumber
         
     }
     
